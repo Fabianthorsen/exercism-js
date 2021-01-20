8f826cdad2b570a0f0a72cd359ebebc3e10edbf3
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-01-20 23:58:37 +0100

    Finished run-length-encoding project

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -3,10 +3,34 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const encode = (string) => {
+  // Declare parameters
+  let n = string.length;
+  let i = 0;
+  let encoded = []; // Will contain encoded string
+
+  while (i < n) {
+    // Count the number of times that string appears in succession
+    let count = 1;
+    while (i < n - 1 && string[i] === string[i + 1]) {
+      count++;
+      i++;
+    }
+    i++;
+    // Report number if it appears more than once in succession
+    if (count > 1) {
+      encoded.push(String(count) + string[i - 1]);
+    } else {
+      encoded.push(string[i - 1]);
+    }
+  }
+  // Return encoding as a string
+  return encoded.join('');
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (string) => {
+  // Use regular expression to map over only int
+  return string.replace(/(\d+)(\w)/g, function (m, n, c) {
+    return new Array(parseInt(n, 10) + 1).join(c);
+  });
 };
