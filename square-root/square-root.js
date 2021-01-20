4c3e5e0dab0fd479ee7e5e4c40cbb8a04fced8ba
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-01-20 21:51:57 +0100

    Finished square-root problem

--- square-root/square-root.js
+++ square-root/square-root.js
@@ -3,6 +3,28 @@
 // convenience to get you started writing code faster.
 //
 
-export const squareRoot = () => {
-  throw new Error('Remove this statement and implement this function');
+// Based on the Newton-Raphson root-finding method
+// Takes in any positive integer N and the approximation/tolerance parameter T
+export const squareRoot = (N, T = 1) => {
+  // If N is negative then throw error and return.
+  if (N < 1) {
+    console.error('Enter positive integer.');
+    return;
+  }
+
+  // Initialize X to be N
+  let X = N;
+
+  while (1) {
+    // Calculate the root aproximation
+    const root = 0.5 * (X + N / X);
+
+    if (Math.abs(root - X) < T) {
+      break;
+    }
+
+    // Update X if (root-X) !< T
+    X = root;
+  }
+  return X;
 };
