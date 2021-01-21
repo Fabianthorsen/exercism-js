f613d58a9d04d8572642c22a02498411872dbf95
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-01-21 13:23:08 +0100

    Finished roman-num project

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRoman = (n) => {
+  // Translation object with all baseline numerals
+  const mapping = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = '';
+
+  // For each number in the translation object,
+  // if the remaining number is larger, append it to the roman translation
+  for (let i in mapping) {
+    while (n >= mapping[i]) {
+      roman += i;
+      n -= mapping[i];
+    }
+  }
+  return roman;
 };
