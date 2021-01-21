7439d30ae35c0da0bd3d99ba59a2adc0b1d99078
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-01-21 11:38:16 +0100

    Finished darts-project

--- darts/darts.js
+++ darts/darts.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
+export const score = (x, y) => {
+  // Declare the three cirlce radius
+  const rInner = 1;
+  const rMid = 5;
+  const rOuter = 10;
+
+  // Assuming that center is 0,0 -> Checking if dart is within each of the circles
+  if (Math.pow(x - 0, 2) + Math.pow(y - 0, 2) <= Math.pow(rInner, 2)) {
+    return 10;
+  } else if (Math.pow(x - 0, 2) + Math.pow(y - 0, 2) <= Math.pow(rMid, 2)) {
+    return 5;
+  } else if (Math.pow(x - 0, 2) + Math.pow(y - 0, 2) <= Math.pow(rOuter, 2)) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
