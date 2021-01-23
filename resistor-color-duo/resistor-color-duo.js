a739199e14a0d10f1179ed333269476634398e55
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-01-23 17:23:09 +0100

    New refactored code for project

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -4,35 +4,23 @@
 //
 
 export const decodedValue = (colorPairs = []) => {
-  // Define color/code pairs
-  const COLORS = {
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9,
-  };
+  // Array of relevant colors
+  const colorArr = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
 
-  // Declare empty list to hold the digits
-  let resistorPair = [];
+  // We only care about the first two
+  const [colorOne, colorTwo, ..._] = colorPairs;
 
-  // Iterate for two input-elements and store the code in variable
-  colorPairs.every((color, idx) => {
-    const lowerColor = color.toLowerCase();
-
-    if (idx < 2) {
-      resistorPair.push(COLORS[lowerColor]);
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  // Return the two-digit code as int
-  return parseInt(resistorPair.join(''));
+  // Return the color-codes
+  return colorArr.indexOf(colorOne) * 10 + colorArr.indexOf(colorTwo);
 };
